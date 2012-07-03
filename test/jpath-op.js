0317746becf4b5a224b2b669e1f147360b4588ce
Artur Burtsev <artjock@gmail.com>
2012-07-03 14:52:50 +0400

    jpath operators

---
+++ test/jpath-op.js
@@ -0,0 +1,45 @@
+var should = require('should');
+var jpath = require('../').jpath;
+
+
+describe('jpath', function() {
+    it('$a | $b -> a | b', function() {
+        jpath('$a | $b').should.equal('a | b');
+    });
+    it('$a + $b -> a + b', function() {
+        jpath('$a + $b').should.equal('a + b');
+    });
+    it('$a - $b -> a - b', function() {
+        jpath('$a - $b').should.equal('a - b');
+    });
+    it('$a div $b', function() {
+        jpath('$a div $b').should.equal('a / b');
+    });
+    it('$a mod $b -> a % b', function() {
+        jpath('$a mod $b').should.equal('a % b');
+    });
+    it('$a or $b -> a || b', function() {
+        jpath('$a or $b').should.equal('a || b');
+    });
+    it('$a and $b -> a && b', function() {
+        jpath('$a and $b').should.equal('a && b');
+    });
+    it('$a &gt; $b -> a > b', function() {
+        jpath('$a &gt; $b').should.equal('a > b');
+    });
+    it('$a &gt;= $b', function() {
+        jpath('$a &gt;= $b').should.equal('a >= b');
+    });
+    it('$a &lt; $b -> a < b', function() {
+        jpath('$a &lt; $b').should.equal('a < b');
+    });
+    it('$a &lt;= $b -> a <= b', function() {
+        jpath('$a &lt;= $b').should.equal('a <= b');
+    });
+    it('$a = $b -> a == b', function() {
+        jpath('$a = $b').should.equal('a == b');
+    });
+    it('$a != $b -> a != b', function() {
+        jpath('$a != $b').should.equal('a != b');
+    });
+});
