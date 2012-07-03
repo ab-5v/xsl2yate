aeea8ddb409cc38ac0917860f64d879631ae38c0
Artur Burtsev <artjock@gmail.com>
2012-07-03 20:50:32 +0400

    i18n

--- test/jpath-complex.js
+++ test/jpath-complex.js
@@ -16,5 +16,11 @@ describe('jpath complex', function() {
     it('count($folders/folder[@user]) > 1', function() {
         jpath('count($folders/folder[@user]) > 1').should.equal('count(folders.folder[.user]) > 1');
     });
+    it('count($folders/folder[@user]) > 1', function() {
+        jpath('count($folders/folder[@user]) > 1').should.equal('count(folders.folder[.user]) > 1');
+    });
+    it('18n(\'Folders_Unread\', new)', function() {
+        jpath('18n(\'Folders_Unread\', new)').should.equal('18n(\'Folders_Unread\', .new)')
+    });
 });
 
