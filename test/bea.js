afcdd21c3044d29ded1daaac8b2baf2c9752dc3b
Artur Burtsev <artjock@gmail.com>
2012-07-04 14:25:54 +0400

    if expr {} else...

--- test/bea.js
+++ test/bea.js
@@ -1,6 +1,6 @@
 var should = require('should');
 var tr = require('../').transform;
-var bea = require('../');
+var bea = require('../lib/bea');
 
 describe('bea', function() {
 
@@ -9,6 +9,12 @@ describe('bea', function() {
     });
     it('%i18n_Setup_Filters_header_if(field, $cond, pattern);', function() {
         tr('%i18n_Setup_Filters_header_if(field, $cond, pattern);').should.eql('{i18n(\'%Setup_Filters_header_if\', .field, cond, .pattern)}');
+    });
+    it('if .some {.a}', function() {
+        bea('    if .some {.a}').should.eql('    if .some {\n        .a\n    }');
+    });
+    it('if \\n else', function() {
+        bea('}\n     else').should.eql('} else');
     });
 });
 
