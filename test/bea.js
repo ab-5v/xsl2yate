c18bde109516a318e6805586e478176e9a61d332
Artur Burtsev <artjock@gmail.com>
2012-07-05 20:49:34 +0400

    i18n

--- test/bea.js
+++ test/bea.js
@@ -5,10 +5,10 @@ var bea = require('../lib/bea');
 describe('bea', function() {
 
     it('%i18n_Setup_Filters_header_1;', function() {
-        tr('%i18n_Setup_Filters_header_1;').should.eql('"{i18n(\'%Setup_Filters_header_1\')}"');
+        tr('%i18n_Setup_Filters_header_1;').should.eql('i18n(\'%Setup_Filters_header_1\')');
     });
     it('%i18n_Setup_Filters_header_if(field, $cond, pattern);', function() {
-        tr('%i18n_Setup_Filters_header_if(field, $cond, pattern);').should.eql('"{i18n(\'%Setup_Filters_header_if\', .field, cond, .pattern)}"');
+        tr('%i18n_Setup_Filters_header_if(field, $cond, pattern);').should.eql('i18n(\'%Setup_Filters_header_if\', .field, cond, .pattern)');
     });
     it('if .some {.a}', function() {
         bea('    if .some {.a}').should.eql('    if .some {\n        .a\n    }');
